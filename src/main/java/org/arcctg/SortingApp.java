3d28316da9eb483d21f438d8473292463b33e625
arcctg <91179695+arcctg@users.noreply.github.com>
2024-10-13 22:55:01 +0300

    Added Javadoc

--- src/main/java/org/arcctg/SortingApp.java
+++ src/main/java/org/arcctg/SortingApp.java
@@ -3,9 +3,37 @@ package org.arcctg;
 import java.util.Arrays;
 
 /**
- * SortingApp is a command-line application that sorts up to ten integers.
+ * <p>SortingApp is a command-line application that sorts up to ten integers provided as arguments.</p>
+ *
+ * <p>If the number of arguments exceeds ten, or if the arguments contain non-integer values,
+ * the application displays an appropriate error message.</p>
+ *
+ * <p>Example usage:</p>
+ * <pre>
+ * -jar sorting-app-project-1.0-SNAPSHOT.jar 3 1 2
+ * Sorted numbers: [1, 2, 3]
+ *
+ * -jar sorting-app-project-1.0-SNAPSHOT.jar 10
+ * Sorted numbers: [10]
+ *
+ * -jar sorting-app-project-1.0-SNAPSHOT.jar
+ * Please provide integers to sort.
+ *
+ * -jar sorting-app-project-1.0-SNAPSHOT.jar 5 4 a
+ * Please provide only integer values.
+ *
+ * -jar sorting-app-project-1.0-SNAPSHOT.jar 1 2 3 4 5 6 7 8 9 10 11
+ * Please provide up to 10 integers.
+ * </pre>
  */
 public class SortingApp {
+    /**
+     * The main method sorts the command-line arguments if they are valid integers and prints the sorted list.
+     *
+     * @param args the command-line arguments, which should be integer values (up to 10 values)
+     *             that will be sorted in ascending order.
+     * @throws NumberFormatException if any of the command-line arguments cannot be parsed as an integer.
+     */
     public static void main(String[] args) {
         if (args.length == 0) {
             System.out.println("Please provide integers to sort.");
--- src/test/java/org/arcctg/SortingAppTest.java
+++ src/test/java/org/arcctg/SortingAppTest.java
@@ -30,8 +30,25 @@ public class SortingAppTest {
                 {new String[]{}, "Please provide integers to sort."},
                 {new String[]{"5"}, "Sorted numbers: [5]"},
                 {new String[]{"10", "3", "7"}, "Sorted numbers: [3, 7, 10]"},
-                {new String[]{"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11"},
-                        "Please provide up to 10 integers."}
+                {new String[]{"19", "1", "68", "24", "5"},
+                        "Sorted numbers: [1, 5, 19, 24, 68]"},
+                {new String[]{"9", "-3", "12", "5", "0", "-8", "14", "-1", "7"},
+                        "Sorted numbers: [-8, -3, -1, 0, 5, 7, 9, 12, 14]"},
+                {new String[]{"12", "5", "0", "-1", "7", "9", "-3", "-8", "14", "1"},
+                        "Sorted numbers: [-8, -3, -1, 0, 1, 5 7, 9, 12, 14]"},
+                {new String[]{"1", "2", "3", "4", "5", "6", "7", "8", "9", "10",
+                        "11"}, "Please provide up to 10 integers."},
+                {new String[]{"3", "4", "15", "-6", "2", "8", "9", "-7", "1",
+                        "0", "13"}, "Please provide up to 10 integers."},
+                {new String[]{"10", "-5", "20", "3", "-2", "7", "4", "6", "-1",
+                        "8", "-9", "2", "13", "0", "11"},
+                        "Please provide up to 10 integers."},
+                {new String[]{"1", "2", "a", "4"},
+                        "Please provide only integer values."},
+                {new String[]{"5", "b", "3"},
+                        "Please provide only integer values."},
+                {new String[]{"@", "7", "8"},
+                        "Please provide only integer values."}
         });
     }
 }
